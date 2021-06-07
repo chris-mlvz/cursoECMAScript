74b98bcb40e8ec32810cc3406a38104a35a82c3e
chris_mlvz <chris.mlvz@gmail.com>
2021-06-07 18:33:20 -0500

    asyn await ES8

--- src/es8/index.js
+++ src/es8/index.js
@@ -4,7 +4,7 @@ const data = {
     frontend: 'Oscar',
     backend: 'Isabel',
     design: 'Ana',
-}
+};
 
 const entries = Object.entries(data);
 console.log(entries);
@@ -15,7 +15,7 @@ const data = {
     frontend: 'Oscar',
     backend: 'Isabel',
     design: 'Ana',
-}
+};
 
 const values = Object.values(data);
 console.log(values);
@@ -24,10 +24,40 @@ console.log(values.length);
 // ! Padding
 const string = 'hello';
 console.log(string.padStart(7, 'hi'));
-console.log(string.padEnd(12, ' -----'))
-console.log('food'.padEnd(12, '  -----'))
+console.log(string.padEnd(12, ' -----'));
+console.log('food'.padEnd(12, '  -----'));
 
 // ! trailing commas
 const obj = {
     name: 'oscar',
-}
+};
+
+// ! Async Await
+// Promise
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'));
+    });
+};
+
+// Async and promise
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    // wait until the promise is resolved
+    console.log(hello);
+};
+helloAsync();
+
+// Async with try/catch
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+anotherFunction();
