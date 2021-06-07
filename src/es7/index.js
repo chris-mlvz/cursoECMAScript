46066dd70072ca60b47aaf7b8019b900819b9f4b
chris_mlvz <chris.mlvz@gmail.com>
2021-06-07 15:37:14 -0500

    es7 implementations

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+
+// ! include
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+    console.log('Yes, the value is found');
+}
+else {
+    console.log('No, the value is not found')
+}
+
+// ! Power
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
